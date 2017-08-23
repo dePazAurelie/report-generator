372a92f134649172d457ec3a4be1c147023894f4
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-08-24 00:44:58 +0200

    Mise en page + utilisation getter

--- src/reportgenerator/main.java
+++ src/reportgenerator/main.java
@@ -1,26 +1,34 @@
 package reportgenerator;
 
-import java.io.FileOutputStream;
-import java.util.ArrayList;
-
-import com.itextpdf.text.Document;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.pdf.PdfWriter;
 import reportgenerator.pdfManager.linker;
 import reportgenerator.pdflayout.pageLayout;
 
 public class main {
 
 	public static void main(String[] args) {
-		try {
-			String FILE = "c:/Users/Aurélie de Paz/Desktop/report-generator/test.pdf";
-			Document document = new Document(PageSize.A4);
-			PdfWriter.getInstance(document, new FileOutputStream(FILE));
-			document.open();
-			document = pageLayout.initDocument(document);
-			document.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
+		linker link = new linker();
+
+		link.addAttendees("Jean", "Paul", "@", "true");
+		link.addAttendees("Jean", "Paul", "@", "true");
+		link.addAttendees("Jean", "Paul", "@", "true");
+		link.addAttendees("Jean", "Paul", "@", "false");
+		link.addAttendees("Jean", "Paul", "@", "false");
+		link.addAttendees("Jean", "Paul", "@", "true");
+		link.addAttendees("Jean", "Paul", "@", "true");
+		link.addAttendees("Jean", "Paul", "@", "true");
+		link.addAttendees("Jean", "Paul", "@", "true");
+
+		link.setProjectName("Report generator");
+		link.setDate("August the 24th 2017");
+		link.addHost("Valentin", "Ronteix", "valentin@ronteix.fr");
+		link.setMeetingNumber("3");
+		link.setNotes("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
+						"ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco" +
+						" laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+						" in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+						" cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
+
+		pageLayout layout = new pageLayout(link);
 	}
 }
--- src/reportgenerator/pdflayout/pageLayout.java
+++ src/reportgenerator/pdflayout/pageLayout.java
@@ -1,26 +1,55 @@
 package reportgenerator.pdflayout;
+import com.itextpdf.text.pdf.draw.LineSeparator;
+import reportgenerator.pdfManager.linker;
+
 import com.itextpdf.text.*;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
-import java.util.Date;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileOutputStream;
 import static com.itextpdf.text.Rectangle.NO_BORDER;
 
 public class pageLayout {
 
-    public static Document initDocument(Document document) {
+    private Document initDocument(Document document, linker link) {
         try {
-            addTitlePage(document);
-            addPeople(document);
+            addTitlePage(document, link);
+            addPeople(document, link);
+            addTopics(document, link);
         } catch (DocumentException e) {
             e.printStackTrace();
         }
         return document;
     }
 
-    private static void addPeople(Document document) throws DocumentException {
+
+    private void addTopics (Document document, linker link) throws DocumentException {
+        Paragraph empty = new Paragraph();
+        addEmptyLine(empty, 1);
+        document.add(empty);
+        Paragraph sectionTitle = new Paragraph("Topics :", subFont);
+        addLineSeparator(sectionTitle);
+        document.add(sectionTitle);
+/*
+        for (int i = 0 ; i < link.getTopics().size() ; i++) {
+            Paragraph topic = new Paragraph();
+            topic.add(new Paragraph(link.getTopics().get(i).getTopic(), smallBold));
+            topic.add(new Paragraph(link.getTopics().get(i).getDecision(), classic));
+            topic.add(new Paragraph(link.getTopics().get(i).getOutcome(), classicRed));
+            addEmptyLine(topic, 1);
+            document.add(topic);
+        }*/
+    }
+
+    private void addPeople(Document document, linker link) throws DocumentException {
+
+        Paragraph sectionTitle = new Paragraph("Attendees :", subFont);
+        addLineSeparator(sectionTitle);
+        document.add(sectionTitle);
+
         PdfPTable table = new PdfPTable(2);
         table.setWidthPercentage(100);
         PdfPCell cell;
@@ -38,64 +67,85 @@ public class pageLayout {
         cell.setRowspan(1);
         table.addCell(cell);
 
-        cell = new PdfPCell(new Phrase("Valentin", classic));
-        cell.setPadding(5f);
-        cell.setBorder(NO_BORDER);
-        cell.setRowspan(1);
-        table.addCell(cell);
-
-        cell = new PdfPCell(new Phrase("Charles", classic));
-        cell.setPadding(5f);
-        cell.setBorder(NO_BORDER);
-        cell.setRowspan(1);
-        table.addCell(cell);
-
-        cell = new PdfPCell(new Phrase("Aurélie", classic));
-        cell.setBorder(NO_BORDER);
-        cell.setPadding(5f);
-        cell.setRowspan(1);
-        table.addCell(cell);
-
-        cell = new PdfPCell(new Phrase("Cécile", classic));
-        cell.setBorder(NO_BORDER);
-        cell.setPadding(5f);
-        cell.setRowspan(1);
-        table.addCell(cell);
+        for (int i = 0 ; i < link.getAttendees().size() ; i++) {
+            cell = new PdfPCell(new Phrase(link.getAttendees().get(i).getFirstName() + " "
+                    + link.getAttendees().get(i).getLastName() + "\n" + link.getAttendees().get(i).getMail() + "\n"
+                    , classic));
+            cell.setPadding(5f);
+            cell.setPaddingBottom(10f);
+            cell.setBorder(NO_BORDER);
+            cell.setRowspan(1);
+            table.addCell(cell);
+        }
         document.add(table);
     }
 
-    private static void addTitlePage(Document document) throws DocumentException {
-        Paragraph preface = new Paragraph();
-        addEmptyLine(preface, 1);
-        preface.add(new Paragraph("Nom du projet, numéro du projet", titleFont));
-        addEmptyLine(preface, 1);
+    private void addTitlePage(Document document, linker link) throws DocumentException {
+        Paragraph header = new Paragraph();
 
-        preface.add(new Paragraph("Sous-titre", subFont));
-        addEmptyLine(preface, 1);
+        Paragraph title = new Paragraph(link.getProjectName(), titleFont);
+        title.setAlignment(Element.ALIGN_CENTER);
+        header.add(title);
 
-        preface.add(new Paragraph("Host : " + System.getProperty("user.name"), smallBold));
-        preface.add(new Paragraph( "E-mail : ", smallBold));
-        addEmptyLine(preface, 1);
-        preface.add(new Paragraph("Date : "+ new Date(), classic));
-       // preface.add(new Paragraph("Durée : ", classic)); V2
-       // preface.add(new Paragraph("Lieu : ", classic)); V2
-        addEmptyLine(preface, 2);
-        document.add(preface);
+        Paragraph number = new Paragraph("meeting n°" + link.getMeetingNumber(), smallBold);
+        number.setAlignment(Element.ALIGN_CENTER);
+        header.add(number);
+
+        addEmptyLine(header, 1);
+        header.add(new Paragraph("Sous-titre", subFont));
+        addEmptyLine(header, 1);
+
+        Phrase phrase = new Phrase();
+        phrase.add(new Chunk("Host : ",smallBold));
+        phrase.add(new Chunk(link.getHost().getFirstName() + " " + link.getHost().getLastName() + "\n", classic));
+        header.add(phrase);
+
+        phrase.clear();
+        phrase.add(new Chunk("E-mail : ",smallBold));
+        phrase.add(new Chunk(link.getHost().getMail() + "\n", classic));
+        header.add(phrase);
+
+        phrase.clear();
+        phrase.add(new Chunk("Date : ",smallBold));
+        phrase.add(new Chunk(link.getDate(), classic));
+        header.add(phrase);
+
+       // header.add(new Paragraph("Durée : ", classic)); V2
+       // header.add(new Paragraph("Lieu : ", classic)); V2
+        addEmptyLine(header, 1);
+        document.add(header);
     }
 
-    private static void addEmptyLine(Paragraph paragraph, int number) {
+    private void addEmptyLine(Paragraph paragraph, int number) {
         for (int i = 0; i < number; i++) {
              paragraph.add(new Paragraph(" "));
         }
     }
 
-    private static Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
-    private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD);
-    private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 13, Font.BOLD);
-    private static Font classic = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL);
+    private void addLineSeparator(Paragraph paragraph) {
+        LineSeparator line = new LineSeparator();
+        line.setOffset(-10f);
+        paragraph.add(line);
+        addEmptyLine(paragraph, 1);
+    }
+
+    private static Font titleFont = new Font(Font.FontFamily.HELVETICA, 24, Font.NORMAL);
+    private static Font subFont = new Font(Font.FontFamily.HELVETICA, 16, Font.NORMAL);
+    private static Font smallBold = new Font(Font.FontFamily.HELVETICA, 13, Font.BOLD);
+    private static Font classic = new Font(Font.FontFamily.HELVETICA, 12, Font.NORMAL);
+    private static Font classicRed = new Font(Font.FontFamily.HELVETICA, 12, Font.NORMAL, BaseColor.RED);
 
 
-    public pageLayout(Document document) {
-        initDocument(document);
+    public pageLayout(linker link) {
+        try {
+            String FILE = "c:/Users/Aurélie de Paz/Desktop/report-generator/test.pdf";
+            Document document = new Document(PageSize.A4);
+            PdfWriter.getInstance(document, new FileOutputStream(FILE));
+            document.open();
+            document = initDocument(document, link);
+            document.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
