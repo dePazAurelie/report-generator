abf62002b4fcbbac730c4ca3d3d349a1f2645373
dataonoff <charlilounet3@hotmail.com>
2017-08-23 18:46:53 +0200

    commit

--- src/reportgenerator/main.java
+++ src/reportgenerator/main.java
@@ -1,13 +1,22 @@
 package reportgenerator;
+<<<<<<< HEAD
  
 import reportgenerator.window.Window;
+=======
+>>>>>>> origin/beta
 
 import java.io.FileNotFoundException;
 
 public class main {
+<<<<<<< HEAD
  
     public static void main(String args[]) throws FileNotFoundException {
 
         Window window = new Window();
+=======
+
+    public static void main (String[] args) throws FileNotFoundException {
+
+>>>>>>> origin/beta
     }
 }
