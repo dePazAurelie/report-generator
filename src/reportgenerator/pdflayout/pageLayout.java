09a9713f48b4b80f6e8e03ec05787d4ae2eb5586
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-08-23 17:58:16 +0200

    la liste de présence !

--- src/reportgenerator/pdflayout/pageLayout.java
+++ src/reportgenerator/pdflayout/pageLayout.java
@@ -5,8 +5,8 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
-
 import java.util.Date;
+import static com.itextpdf.text.Rectangle.NO_BORDER;
 
 public class pageLayout {
 
@@ -25,18 +25,42 @@ public class pageLayout {
         table.setWidthPercentage(100);
         PdfPCell cell;
 
-        cell = new PdfPCell(new Phrase("Cell with colspan 3"));
+        cell = new PdfPCell(new Phrase("Presents :", smallBold));
+        cell.setPadding(10f);
+        cell.setPaddingLeft(5f);
+        cell.setBorder(NO_BORDER);
         cell.setColspan(1);
         table.addCell(cell);
 
-        cell = new PdfPCell(new Phrase("Cell with rowspan 2"));
+        cell = new PdfPCell(new Phrase("Excused :", smallBold));
+        cell.setPadding(5f);
+        cell.setBorder(NO_BORDER);
         cell.setRowspan(1);
         table.addCell(cell);
-        // we add the four remaining cells with addCell()
-        table.addCell("row 1; cell 1");
-        table.addCell("row 1; cell 2");
-        table.addCell("row 2; cell 1");
-        table.addCell("row 2; cell 2");
+
+        cell = new PdfPCell(new Phrase("Valentin", classic));
+        cell.setPadding(5f);
+        cell.setBorder(NO_BORDER);
+        cell.setRowspan(1);
+        table.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Charles", classic));
+        cell.setPadding(5f);
+        cell.setBorder(NO_BORDER);
+        cell.setRowspan(1);
+        table.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Aurélie", classic));
+        cell.setBorder(NO_BORDER);
+        cell.setPadding(5f);
+        cell.setRowspan(1);
+        table.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Cécile", classic));
+        cell.setBorder(NO_BORDER);
+        cell.setPadding(5f);
+        cell.setRowspan(1);
+        table.addCell(cell);
         document.add(table);
     }
 
@@ -52,10 +76,10 @@ public class pageLayout {
         preface.add(new Paragraph("Organisateur : " + System.getProperty("user.name"), smallBold));
         preface.add(new Paragraph( "email", smallBold));
         addEmptyLine(preface, 1);
-        preface.add(new Paragraph("Date : "+ new Date(), smallBold));
-        preface.add(new Paragraph("Durée : ", smallBold));
-        preface.add(new Paragraph("Lieu : ", smallBold));
-        addEmptyLine(preface, 4);
+        preface.add(new Paragraph("Date : "+ new Date(), classic));
+        preface.add(new Paragraph("Durée : ", classic));
+        preface.add(new Paragraph("Lieu : ", classic));
+        addEmptyLine(preface, 2);
         document.add(preface);
     }
 
@@ -66,9 +90,9 @@ public class pageLayout {
     }
 
     private static Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
-    private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.RED);
     private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD);
-    private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+    private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 13, Font.BOLD);
+    private static Font classic = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL);
 
 
     public pageLayout(Document document) {
