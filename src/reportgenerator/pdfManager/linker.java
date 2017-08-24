a1cad4ccdafe636a345e869e77af72b0c58a6d64
VyrtualSynthese <vyrtualsynthese@gmail.com>
2017-08-24 12:33:19 +0200

    gestion des multiple presents et absents

--- src/reportgenerator/pdfManager/linker.java
+++ src/reportgenerator/pdfManager/linker.java
@@ -8,6 +8,8 @@ public class linker {
     private String meetingNumber;
     private String date;
     private attendee host;
+    private ArrayList <attendee> attendeesPresents = new ArrayList<>();
+    private ArrayList <attendee> attendeesExcused = new ArrayList<>();
     private ArrayList <attendee> attendees = new ArrayList<>();
     private ArrayList <topic> topics = new ArrayList<>();
     private String notes;
@@ -49,8 +51,43 @@ public class linker {
         return attendees;
     }
 
+
+    //Création d'array differents contenant respectivement les présents et les absents.
     public void addAttendees(String firstName, String lastName, String mail, String status) {
-        attendees.add(new attendee(firstName, lastName, mail, status));
+        if (status == "checked"){
+            attendeesPresents.add(new attendee(firstName, lastName, mail, status));
+        }
+        else {
+            attendeesExcused.add(new attendee(firstName, lastName, mail, status));
+        }
+    }
+
+
+    /**Fait en sorte que les deux array contenant les Presents et Excused font la même taille et sont remplis par des null puis va transvaser les deux array en alternant dans l'arrayList attendees**/
+    public void mergeAttendees(){
+
+        while (attendeesPresents.size() != attendeesExcused.size()) {
+
+            if (attendeesPresents.size() < attendeesExcused.size()) {
+
+                attendeesPresents.add(new attendee("", "", "", ""));
+
+            } else if (attendeesPresents.size() > attendeesExcused.size()) {
+
+                attendeesExcused.add(new attendee("", "", "", ""));
+
+            } else {
+                System.out.print("error on mergeAttendees");
+                        break;
+            }
+        }
+
+        for (int i = 0; i < attendeesPresents.size(); i++) {
+
+            attendees.add(attendeesPresents.get(i));
+            attendees.add(attendeesExcused.get(i));
+
+        }
     }
 
     public ArrayList<topic> getTopics() {
